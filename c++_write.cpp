a0cf4dc34dd7b4a7a16f2ec30fc4c2cade443caf
ro4438 <37229913+ro4438@users.noreply.github.com>
2020-05-08 02:56:00 -0500

    Create c++_write.cpp

---
+++ c++_write.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    string message, filename;
+    cout << "Enter your message: ";
+    getline(cin, message);
+    cout << "Enter file name: ";
+    cin >> filename;
+    if (filename.substr(filename.length()-4, filename.length()-1) != ".txt") {
+        filename = filename + ".txt";
+    }
+    ofstream myfile;
+    myfile.open(filename);
+    myfile << message;
+    myfile.close();
+
+    return 0;
+}
