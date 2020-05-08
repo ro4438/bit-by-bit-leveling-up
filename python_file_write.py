d3dc91dfedd27bb05c71f1d2ba616cd8e65ae97d
ro4438 <37229913+ro4438@users.noreply.github.com>
2020-05-08 02:55:37 -0500

    Create python_file_write.py

---
+++ python_file_write.py
@@ -0,0 +1,10 @@
+def write_to_file(filename, txt):
+    with open(filename, 'w') as file_object:
+        s = file_object.write(txt)
+
+if __name__ == '__main__':
+  message = str(input("Enter your message: "))
+  filename = str=(input("Enter file name: "))
+  if filename[-4:] != ".txt":
+    filename = filename + ".txt"
+  write_to_file(filename, message)
