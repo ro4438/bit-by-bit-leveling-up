b3d50b66834dfd89e68941f8b715020139d8162e
ro4438 <37229913+ro4438@users.noreply.github.com>
2020-05-08 12:17:15 -0500

    Update python_file_write.py

--- python_file_write.py
+++ python_file_write.py
@@ -3,8 +3,9 @@ def write_to_file(filename, txt):
         s = file_object.write(txt)
 
 if __name__ == '__main__':
-  message = str(input("Enter your message: "))
-  filename = str=(input("Enter file name: "))
-  if filename[-4:] != ".txt":
-    filename = filename + ".txt"
-  write_to_file(filename, message)
+    print("Python File Writer Starting.")
+    message = str(input("Enter your message: "))
+    filename = str=(input("Enter file name: "))
+    if filename[-4:] != ".txt":
+        filename = filename + ".txt"
+    write_to_file(filename, message)
